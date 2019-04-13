504de4e469f7734a61f6053b1991bcf325df6b8e
Alexa Gibson <alexa.gibson@outlook.com>
2019-04-13 15:45:13 +0000

    Added Procfile for deployment

--- run.py
+++ run.py
@@ -1,32 +1,45 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
-def add_messages(username, message):
+
+def add_message(username, message):
     """ Add messages to the messages list """
-    messages.append("{}: {}".format(username, message))
-    
-def get_all_messages():
-    """Get all of the messages and separate them with a br """
-    return "<br>".join(messages)
+    now = datetime.now().strftime("%H:%M:%S")
+    message = message.capitalize()
+    messages.append({"timestamp": now, "from": username, "message": message})
     
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     """ Main page with instructions """
-    return "To send a message use /USERNAME/MESSAGE"
     
-@app.route("/<username>")
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+    
+    return render_template("index.html")
+    
+@app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
-    """ Display chat messages """
-    return "<h1>Welcome, {0}</h1> {1}".format(username.capitalize(), get_all_messages())
+    username = username.capitalize()
+    """ Add and display chat messages """
+    if request.method == "POST":
+        username = session["username"]
+        username = username.capitalize()
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
     
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    """ Create new message and redirect to the chat page """
-    add_messages(username, message)
-    return redirect("/" + username)
-    
-app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
+    return render_template("chat.html", username = username, chat_messages = messages)
+
+
+
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=False)
+
